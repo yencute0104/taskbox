f5f2810f752ba83c5ad8acfdc26dfdfa79e95176
yen tang <yencute0104@gmail.com>
2024-03-11 14:36:58 +0700

    test docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,16 @@
+import '../src/index.css';
+
+// Registers the msw addon
+import { initialize, mswLoader } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize();
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -9,6 +18,7 @@ const preview = {
       },
     },
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
